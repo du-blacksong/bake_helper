2c111f4460b8b783bddcbdbfe0894ae1f1ee01c6
yansuowhb <2081720759@qq.com>
2020-10-06 23:27:42 +0800

    完成video页面,拆分几个组件

---
+++ src/components/videoScroll/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="recommend">
+    <div class="title">{{title}}</div>
+    <div class="pic-wrapper" ref="wrapper">
+      <div class="pic-list">
+        <div class="pic-item" v-for="item in recommendList" :key="item.contentId">
+          <img :src="item.coverImage">
+          <div class="learnNum">1000+在学</div>
+          <div class="itemTitle">{{item.coverTitle}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+export default {
+name: "index",
+  props:{
+  title:{
+    type:String,
+    required:true
+  },
+    recommendList:{
+      type:Array,
+      required:true
+    }
+  },
+  beforeMount () {
+    this.$nextTick(()=>{
+      new BScroll(this.$refs.wrapper,{
+        click:true,
+        scrollX:true,
+      })
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+//  推荐课程
+.recommend {
+  .title {
+    padding: 30px 30px 20px;
+    line-height: 50px;
+    font-size: 32px;
+    font-weight: bold;
+  }
+
+  .pic-wrapper {
+    width: 100%;
+    margin:0 30px;
+    overflow: hidden;
+    .pic-list{
+      display: flex;
+      width: 3400px;
+      padding-right: 30px;
+      .pic-item{
+        width: 320px;
+        height: 540px;
+        margin-right: 20px;
+        position: relative;
+        img{
+          width: 320px;
+          height: 450px;
+          border-radius: 8px;
+        }
+        .learnNum{
+          position: absolute;
+          left: 0;
+          bottom: 107px;
+          font-size: 22px;
+          margin: 0 20px;
+          padding: 4px 8px;
+          border-radius: 8px;
+          color: #4A4945;
+          background-color: #fff;
+
+        }
+        .itemTitle{
+          font-size: 30px;
+          line-height: 40px;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2; //文本行数
+        }
+
+      }
+
+    }
+  }
+}
+</style>

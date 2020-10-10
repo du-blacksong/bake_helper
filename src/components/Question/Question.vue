81f60b54f9ee003a61c27508a3bc69f1a62b825f
chenxiang <625815152@qq.com>
2020-10-10 19:46:43 +0800

    小王提交

--- src/components/Question/Question.vue
+++ src/components/Question/Question.vue
@@ -1,73 +1,74 @@
 <!--最新问题和最热问题共用组件封装-->
 <template>
-<div class="outer">
-  <div class="questionsWrap" v-for="(item, index) in news" :key="index">
+  <div class="outer">
+    <div class="questionsWrap" v-for="(item, index) in news" :key="index">
       <div>
-          <a class="questionTitle" href="">{{item.coverTitle}}</a>
+        <a class="questionTitle" href="">{{item.coverTitle}}</a>
       </div>
       <div @click="toFoodDetail(item.recipeContentId)" class='questionsContent'>
-          <img :src="item.recipe.image" alt="" class='imgWrap'>
-          <div class="rightContent">
-              <div class='contentTitle'>{{item.recipe.title}}</div>
-              <div class="author">作者：{{item.recipe.clientName}}</div>
-          </div>
+        <img :src="item.recipe.image" alt="" class='imgWrap'>
+        <div class="rightContent">
+          <div class='contentTitle'>{{item.recipe.title}}</div>
+          <div class="author">作者：{{item.recipe.clientName}}</div>
+        </div>
       </div>
       <div class='questionsBottom'>
-            <div class='answerNumber'>{{item.answerNum ===0? `暂无回答 ` :item.answerNum+'个回答'}}</div>
-          <img src="https://image.hongbeibang.com/FlSZI5KwZLrR9-QXD9Vu7u0lVvCE?48X48&imageView2/1/w/40/h/40" alt="">
-          <div class='answerWrite'>写答案</div>
+        <div class='answerNumber'>{{item.answerNum ===0? `暂无回答 ` :item.answerNum+'个回答'}}</div>
+        <img src="https://image.hongbeibang.com/FlSZI5KwZLrR9-QXD9Vu7u0lVvCE?48X48&imageView2/1/w/40/h/40" alt="">
+        <div class='answerWrite'>写答案</div>
       </div>
-  </div>
-  <div id="flag">
-    <img src="../../static/images/loding.gif">
+    </div>
+    <div id="flag">
+      <img src="../../static/images/loding.gif">
       加载中...
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import BetterScroll from 'better-scroll'
-import router from "../../router"
-export default {
+  import BetterScroll from 'better-scroll'
+  import router from "../../router"
+  export default {
     name: 'Question',
-    props:{
-        news:{
-            type:Array
-        },
-        method:{
-            type:Function
-        }
+    props: {
+      news: {
+        type: Array
+      },
+      method: {
+        type: Function
+      }
     },
-    mounted () {
-    window.addEventListener("scroll",this.handleScroll)
+    mounted() {
+      window.addEventListener("scroll", this.handleScroll)
     },
     methods: {
-          //  无限滚动的回调
-    handleScroll(e) {
-  const flag=document.querySelector('#flag')
-      // console.log("top",top)
-      // console.log("bottom",flag.getBoundingClientRect().bottom)
-  //    元素底部距离视口顶部的距离
-//   console.log(~~(flag.getBoundingClientRect().top));
-  if (~~(flag.getBoundingClientRect().top)<725){
-    this.method()
-  }
-},
-toFoodDetail(contentId){
-    this.$router.push(`/foodDetail?contentId=${contentId}`)
-    console.log(111)
-}
+      //  无限滚动的回调
+      handleScroll(e) {
+        const flag = document.querySelector('#flag')
+        // console.log("top",top)
+        // console.log("bottom",flag.getBoundingClientRect().bottom)
+        //    元素底部距离视口顶部的距离
+        //   console.log(~~(flag.getBoundingClientRect().top));
+        if (~~(flag.getBoundingClientRect().top) < 725) {
+          this.method()
+        }
+      },
+      toFoodDetail(contentId) {
+        this.$router.push(`/foodDetail?contentId=${contentId}`)
+        console.log(111)
+      }
     },
     // 离开页面销毁监听事件；
-  destroyed() {
-    window.removeEventListener("scroll", this.handleScroll, false);
-  },
-}
+    destroyed() {
+      window.removeEventListener("scroll", this.handleScroll, false);
+    },
+  }
 </script>
 <style scoped lang="scss">
-.outer{
-    padding-bottom :100px;
-.questionsWrap{
+  .outer {
+    padding-bottom: 100px;
+
+    .questionsWrap {
       position: relative;
       overflow: hidden;
       zoom: 1;
@@ -77,106 +78,116 @@ toFoodDetail(contentId){
       margin: 0 auto;
       box-sizing: border-box;
       padding: 30px 0 20px 0;
-    .questionTitle{
-      font-size:36px;
-      color: #4A4945;
-     font-weight: bold;
-      text-decoration: none;
-    }
-    .questionsContent{
-      position: relative;
-      overflow: hidden;
-      zoom: 1;
-      background-color: #F5F7F9;
-      box-sizing: border-box;
-      border-radius: 8px;
-      display: flex;
-      margin-bottom: 10px;
-      margin-top: 10px;
 
-      .imgWrap{
+      .questionTitle {
+        font-size: 36px;
+        color: #4A4945;
+        font-weight: bold;
+        text-decoration: none;
+      }
+
+      .questionsContent {
+        position: relative;
+        overflow: hidden;
+        zoom: 1;
+        background-color: #F5F7F9;
+        box-sizing: border-box;
+        border-radius: 8px;
+        display: flex;
+        margin-bottom: 10px;
+        margin-top: 10px;
+
+        .imgWrap {
           width: 140px;
           height: 140px;
           border-radius: 8px;
+        }
       }
-    }
-    .rightContent{
-      display: inline-flex;
-      flex: 1;
-      flex-direction: column;
-      justify-content: center;
-      margin-left: 30px;
-      .contentTitle{
+
+      .rightContent {
+        display: inline-flex;
+        flex: 1;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: 30px;
+
+        .contentTitle {
+          font-size: 28px;
+          color: #313131;
+          font-weight: bold;
+          text-align: justify;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          display: -webkit-box;
+          -webkit-line-clamp: 1;
+          -webkit-box-orient: vertical;
+          word-break: break-all;
+        }
+
+        .author {
+          margin-top: 10px;
+          font-size: 26px;
+          color: #999;
+          text-align: justify;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          display: -webkit-box;
+          -webkit-line-clamp: 1;
+          -webkit-box-orient: vertical;
+          word-break: break-all;
+        }
+      }
+
+      .questionsBottom {
+        display: flex;
+        overflow: hidden;
         font-size: 28px;
-        color: #313131;
-        font-weight: bold;
-        text-align: justify;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 1;
-        -webkit-box-orient: vertical;
-        word-break: break-all;
-      }
-      .author{
-        margin-top: 10px;
-        font-size: 26px;
-        color: #999;
-        text-align: justify;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 1;
-        -webkit-box-orient: vertical;
-        word-break: break-all;
-      }
-    }
-    .questionsBottom{
-      display: flex;
-      overflow: hidden;
-      font-size: 28px;
-      line-height: 20px;
-      color: #999999;
-      box-sizing: border-box;
-      margin-top: 20px;
-      .answerNumber{
+        line-height: 20px;
+        color: #999999;
+        box-sizing: border-box;
+        margin-top: 20px;
+
+        .answerNumber {
           color: #498DD0;
           margin-top: 15px;
-          margin-bottom:15px;
-          flex:1
+          margin-bottom: 15px;
+          flex: 1
+        }
+
+        img {
+          width: 38px;
+          height: 38px;
+          display: inline-block;
+          vertical-align: middle;
+          margin-right: 10px;
+          font-size: 0;
+        }
+
+        .answerWrite {
+          font-size: 28px;
+          line-height: 40px;
+          vertical-align: middle;
+          display: inline-block;
+          color: #E98B71;
+        }
       }
-      img{
-       width: 38px;
-       height: 38px;
-       display: inline-block;
-       vertical-align: middle;
-       margin-right: 10px;
-       font-size: 0;
-      }
-      .answerWrite{
-        font-size: 28px;
-        line-height: 40px;
-        vertical-align: middle;
-        display: inline-block;
-        color: #E98B71;
-      }
+
     }
 
-}
-
 
   }
+
   #flag {
-  width: 100%;
-  height: 63px;
-  line-height: 63px;
-  text-align: center;
-  font-size: 32px;
-  background: #F5F7F9;
-  img{
-    width: 40px;
-    height: 40px;
-  }
-}
+    width: 100%;
+    height: 63px;
+    line-height: 63px;
+    text-align: center;
+    font-size: 32px;
+    background: #F5F7F9;
 
+    img {
+      width: 40px;
+      height: 40px;
+    }
+  }
 </style>
--- src/views/foodDetail/foodDetail.vue
+++ src/views/foodDetail/foodDetail.vue
@@ -16,24 +16,37 @@
                 <div class="addFocus">+关注</div>
               </div>
           </div>
-            <p>{{recipe.coverSummary}}</p>
-          <div>
-              <div></div>
-              <div>
-                  <div></div>
-                  <input type="text">
-                  <div></div>
-                  <span></span>
-                  <span></span>
+          <p>{{recipe.coverSummary}}</p>
+          <div class="foodStep">
+              <div calss="foodStepUser" style="margin-bottom:20px">食材用料</div>
+              <div class="foodCount">
+                  <div class="foodDic">
+                    <i>-</i>
+                  </div>
+                  <input type="text" placeholder="8">
+                  <div class="foodAdd">
+                      <i>+</i>
+                 </div>
+                  <span>(份量/</span>
+                  <span>{{recipe.unit}})</span>
               </div>
-              <div>
-                  <div></div>
+              <div class="foodStepCount" v-for="(item, index) in recipe.material" :key="index">
+                  <div>{{item.name}}</div>
+                  <div>{{item.weight}}</div>
               </div>
-              <div>
-                  <div></div>
-                  <img src="" alt="">
-                  <p></p>
+              <div calss="foodStepDetail" v-for="(item,index) in recipe.step" :key="item.contentRecipeStepId">
+                  <div class="step" style="margin:20px 0;">步骤{{1 + index++}}</div>
+                  <img :src="item.image" alt="">
+                  <p>{{item.text}}</p>
               </div>
+          </div>
+          <div class="foodTip">
+              <div class="tip" style="font-size:18px;">小贴士</div>
+              <p>{{recipe.tip}}</p>
+          </div>
+          <div class="foodHomework">
+              <div calss="homeWork" style="font-size:18px;font-weight: bold; margin-right:230px">作业</div>
+              <div class="watchAll" style="font-size:15px;color:#999">查看全部</div>
           </div>
       </div>
   </div>
@@ -60,7 +73,7 @@ export default {
     //   this.contentId=contentId
       const {data} =await this.$axios(`/recipe/get?contentId=${contentId}`)
       this.recipe=data.data.recipe
-      console.log(this.recipe)
+      console.log(this.recipe.coverSummary.split('↵↵'))
     //   this.recipe=data.recipe
     //   console.log(this.recipe)
   }
@@ -123,23 +136,163 @@ export default {
        margin: 32px 0;
 
       }
+     p{
+          color: #313131;
+    line-height: 54px;
+    font-weight: 200;
+    font-size: 34px;
+    word-break: break-all;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    word-wrap: break-word;
+      }
       .foodDetailContent{
        border-top: 2px solid #F1F1F1;
-       margin-top: 30px;
+       padding-top: 30px;
+    //    margin-bottom:30px;
        display: flex;
        img{
            width:70px;
            height: 70px;
+           border-radius: 50%;
        }
        .foodDetailContentRight{
            display: flex;
            flex: 1;
+           margin-left:25px;
            justify-content:space-between;
             .foodDetailName{
+                font-size: 28px;
+                height: 80px;
+                line-height: 80px;
+                color: #4A4945;
+                overflow: hidden;
+                text-overflow: ellipsis;
             }
             .addFocus{
+                background-color: #E98B71;
+                color: #ffff;
+                border: 1px solid;
+                border-color: #E98B71;
+                border-radius: 8px;
+                border-radius: 40px;
+                box-sizing: border-box;
+                margin: 0;
+                text-align: center;
+                font-size: 28px;
+                line-height: 60px;
+                width: 130px;
+                height: 60px;
+                cursor: pointer
             }
        }
+      }
+      .foodStep{
+          padding-top: 30px;
+          font-size: 32px;
+          line-height: 45px;
+          color: #313131;
+          font-weight: bold;
+        .foodStepCount{
+            display: flex;
+            margin-top: 40px;
+            div{
+                flex: 1;
+                justify-content:center;
+                margin-right: 30px;
+                font-size: 34px;
+                line-height: 54px;
+                color: #313131;
+                font-weight: 200;
+            }
+        }
+        .foodStepUser{
+           zoom: 1;
+           margin-bottom: 40px;
+          }
+        .foodCount{
+            display:flex;
+            span{
+                color: #999;
+                font-size:26px;
+                font-weight: normal
+            }
+            input{
+                line-height: 56px;
+                border: 0;
+                border-radius: 0;
+                text-align: center;
+                display: inline-block;
+                box-sizing: border-box;
+                outline: none;
+                vertical-align: middle;
+                margin-left: 10px;
+                margin-right: 10px;
+                width: 70px;
+                padding: 0 15px;
+                height: 70px;
+                border-radius: 8px;
+                font-size: 34px;
+                font-weight: bold;
+                color: #313131;
+                background-color: #F5F7F9;
+                border-style: none;
+                border-bottom-style: solid;
+                border-bottom-width: thin;
+                border-bottom-color: #4a4640;
+            }
+            .foodDic{
+             font-size: 20px;
+             height: 70px;
+             width: 70px;
+             text-align: center;
+             border-radius: 8px;
+             background-color: #F5F7F9;
+             padding: 10px;
+             display: inline-block;
+             vertical-align: middle;
+             box-sizing: border-box;
+             position: relative;
+             margin-right: 10px;
+             i{
+                 color:#999;
+                 
+             }
+            }
+            .foodAdd{
+             font-size: 20px;
+             height: 70px;
+             width: 70px;
+             text-align: center;
+             border-radius: 8px;
+             background-color: #F5F7F9;
+             padding: 10px;
+             display: inline-block;
+             vertical-align: middle;
+             box-sizing: border-box;
+             position: relative;
+             margin-right: 10px;
+             i{
+                 color:#999;
+                 
+             }
+            }
+        }
+        .foodStepDetail{
+           margin-top:100px
+        }
+      }
+      .foodHomework{
+          display:flex;
+          .homeWork{
+              flex: 1;
+          }
+          .watchAll{
+            zoom: 1;
+          }
       }
     }
 }
